9a2ff36ea0e10f863b17a78bd92993ba7a1b4fc1
laure-bonnevalle <laure.bonnevalle@orange>
2024-06-20 15:48:51 +0000

    traduction blog secu

---
+++ controllers/AbstractController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author : Gaellan
+ * @link : https://github.com/Gaellan
+ */
+
+
+abstract class AbstractController
+{
+    
+    protected Translator $translator;
+
+    public function __construct(string $file, protected string $currentLang = "en")
+    {
+        $this->translator = new Translator($file, $this->currentLang);
+    }
+    
+    protected function render(string $template, array $data) : void
+    {
+        require "templates/layout.phtml";
+    }
+
+    protected function redirect(string $route) : void
+    {
+        header("Location: $route");
+    }
+}
+    
+    
